32a55a1b27b22c4f350ed2af9cf3d9d3179d3aee
Martin Holst Swende <martin@swende.se>
2021-09-02 09:22:43 +0200

    cmd/evm: add tests for evm t8n (#23507)
    This commit applies go-ethereum commit 4d88974864c3ee84a24b1088064162b8dbab8ad5

--- cmd/evm/t8n_test.go
+++ cmd/evm/t8n_test.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/celo-org/celo-blockchain/internal/cmdtest"
 	"github.com/docker/docker/pkg/reexec"
-	"github.com/ethereum/go-ethereum/internal/cmdtest"
 )
 
 func TestMain(m *testing.M) {
